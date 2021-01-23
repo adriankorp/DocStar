5d1ce0ed5952736d07fa66b3241cbecb555cdae3
Adrian <adis112211@gmail.com>
2021-01-23 09:32:51 +0100

    fix: urls.py home

--- DocStar/urls.py
+++ DocStar/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('app_viewer.urls'))
+    path('', include('app_viewer.urls'))
 ]
