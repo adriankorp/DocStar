8edd9bd52bedf8dfd59c744faa766ff37ee06835
krzys <krzysztof.domozych@gmail.com>
2021-01-23 12:32:19 +0100

    app_forms.py v_3

--- app_accounts/forms.py
+++ app_accounts/forms.py
@@ -3,6 +3,8 @@ from django import forms
 
 class RegisterForm(forms.Form):
     user = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    surename = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
     password_repeat = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
