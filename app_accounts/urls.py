f0561287b40fe410e29e7cc9f47e71bfe22ed740
Adrian <adis112211@gmail.com>
2021-01-24 10:44:20 +0100

    features: add login form

--- app_accounts/urls.py
+++ app_accounts/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from app_accounts.views import MyLoginView
+
+app_name = 'app_accounts'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app_accounts.urls')),
-    path('', include('app_viewer.urls')),
-
+    path('', MyLoginView.as_view(), name='login'),
 ]
--- app_accounts/views.py
+++ app_accounts/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+class MyLoginView(LoginView):
+    template_name = 'form_login.html'
