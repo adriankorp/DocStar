1562f292876dff56511085ef075f3c59f0b7193c
Asia Morawska <asimorawska@gmail.com>
2021-01-24 09:41:25 +0100

    refactor of Modules

--- app_accounts/models.py
+++ app_accounts/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from django.db.models import Model, OneToOneField, DO_NOTHING, CharField, BooleanField, AutoField, ForeignKey, \
+    EmailField
+
+
+class Profile(Model):
+    user = OneToOneField(User, on_delete=DO_NOTHING, primary_key=True)
+    password = CharField(max_length=128)
+    is_pro = BooleanField(default=False)
+    name = CharField(max_length=128)
+    surname = CharField(max_length=128)
+    user_id = AutoField(primary_key=True)
+
+
+class ContactBook(Model):
+    user_id = ForeignKey(Profile, on_delete=DO_NOTHING)
+    name = CharField(max_length=128)
+    surname = CharField(max_length=128)
+    email = EmailField()
